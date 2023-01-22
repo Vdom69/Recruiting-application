7750b722c9f746f14d5027c66d8575e6336c2230
clevercattv <clevercattv@gmail.com>
2023-01-22 18:30:57 +0200

    Select IndexedDB as storage for application

--- src/data/CandidatesRepository.js
+++ src/data/CandidatesRepository.js
@@ -1,19 +1,21 @@
 const candidatesKey = "candidates";
 
-// todo read this https://developer.chrome.com/blog/deprecating-web-sql/
-//  and this https://developer.chrome.com/blog/sqlite-wasm-in-the-browser-backed-by-the-origin-private-file-system/
-//  investigate how sqlite will work with "Origin Private File System", and can we use it in the extension
+/**
+ * Why was selected "IndexedDB (idb)":
+ * 1. https://stackoverflow.com/questions/69846971/chrome-extension-development-chrome-storage-local-vs-indexeddb
+ * 2. File System Access API was ignored, because it didn't save the session of the access to the files.
+ *     So user need to confirm the access to the files everytime he opens the browser
+ * Other info about it:
+ * https://developer.chrome.com/articles/file-system-access/
+ */
 class CandidatesRepository {
 
     findCandidate(id) {
 
-        return chrome.storage.local.get(candidatesKey)
-            .then(candidates => candidates);
     }
 
     findAllCandidates() {
 
-        return chrome.storage.local.get(candidatesKey);
     }
 
     saveCandidate(candidate) {
